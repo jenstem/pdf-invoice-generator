2c890f456578e72517d86c628c6c07ad4af950fc
Jen <jennifer.stemkowski@gmail.com>
2024-04-17 11:21:15 -0400

    Add button for adding products

--- invoice.py
+++ invoice.py
@@ -4,6 +4,7 @@ root = Tk()
 root.title("Invoice Generator")
 
 
+# Medicine
 medicine_label = Label(root, text="Medicine: ")
 medicine_label.pack()
 
@@ -11,10 +12,16 @@ medicine_listbox = Listbox(root, selectmode=SINGLE)
 medicine_listbox.pack()
 
 
+# Quantity
 quantity_label = Label(root, text="Quantity: ")
 quantity_label.pack()
 quantity_entry = Entry(root)
 quantity_entry.pack()
+
+
+# Button
+add_button = Button(root, text="Add Medicine")
+add_button.pack()
 
 
 root.mainloop()
