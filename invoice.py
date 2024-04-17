bc0834d82243d7533712eef85aad18262349be73
Jen <jennifer.stemkowski@gmail.com>
2024-04-17 11:19:49 -0400

    Add medicine label + listbox, quantity label + entry

--- invoice.py
+++ invoice.py
@@ -3,4 +3,18 @@ from tkinter import *
 root = Tk()
 root.title("Invoice Generator")
 
+
+medicine_label = Label(root, text="Medicine: ")
+medicine_label.pack()
+
+medicine_listbox = Listbox(root, selectmode=SINGLE)
+medicine_listbox.pack()
+
+
+quantity_label = Label(root, text="Quantity: ")
+quantity_label.pack()
+quantity_entry = Entry(root)
+quantity_entry.pack()
+
+
 root.mainloop()
